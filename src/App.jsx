8ba541c48c1c453d8f4c3049277f8240817eb0ca
kei220324 <0vx05845724272k@ezweb.ne.jp>
2026-04-09 17:49:31 +0900

    envを追加しAPIのベースURLを環境変数で管理

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProjectListPage from "./pages/ProjectListPage";
 import ProjectDetailPage from "./pages/ProjectDetailPage";
+import ProjectCreatePage from "./pages/ProjectCreatePage";
+import ProjectEditPage from "./pages/ProjectEditPage";
 
 
 function App() {
@@ -10,6 +12,9 @@ function App() {
         <Routes>
           <Route path="/projects" element={<ProjectListPage />} />
            <Route path="/projects/:projectId" element={<ProjectDetailPage />} />
+           <Route path="/projects/create" element={<ProjectCreatePage />} />
+          <Route path="/projects/:projectId/edit" element={<ProjectEditPage />} />
+
         </Routes>
       </BrowserRouter>
     </>
@@ -17,3 +22,5 @@ function App() {
 }
 
 export default App;
+
+
