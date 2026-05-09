75809ea341b2e620cbf2c9b330e991bf7b385373
kei220324 <0vx05845724272k@ezweb.ne.jp>
2026-05-09 13:56:48 +0900

    不要な箇所を削除

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -13,15 +13,15 @@ export default function HomePage() {
       <div className="homeCard">
         <h1 className="homeTitle">プロジェクト管理システム</h1>
 
-        <p className="homeSubtitle">プロジェクトとタスクをシンプルに管理</p>
-
-        <p className="homeDescription">
-          React と Laravel API を使ったプロジェクト管理アプリです。
-          <br />
-          タスクの作成・編集・削除、ステータス管理ができます。
+        <p className="homeSubtitle">
+          プロジェクトとタスクをシンプルに管理
         </p>
 
-        <button type="button" className="homeButton" onClick={handleGoToProjects}>
+        <button
+          type="button"
+          className="homeButton"
+          onClick={handleGoToProjects}
+        >
           アプリを見る
         </button>
       </div>
